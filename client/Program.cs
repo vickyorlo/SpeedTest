effc9a2cda7e1c81b6d936a81ca1fef5aedc83f2
Vic <vickyorlo@gmail.com>
2018-01-21 04:11:27 +0100

    Initial commit

---
+++ client/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace client
+{
+    class Program
+    {
+        public static IPAddress server = IPAddress.Parse("127.0.0.1");
+        public static int port = 25565;
+        public static int DefaultBufferSize = 1024;
+        public static int bufferSize = 1024;
+        public static bool useNagle = true;
+
+        public static int messageLengthInBuffers = 1000;
+        static void Main(string[] args)
+        {
+            try
+            {
+                server = IPAddress.Parse(args[0]);
+                port = Int32.Parse(args[1]);
+                messageLengthInBuffers = Int32.Parse(args[2]);
+                useNagle = (args[3] == "yes");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid arguments!");
+            }
+
+            List<byte[]> message = PrepareMessage();
+
+            Task.WaitAll(
+            Task.Run(() => TCPSend(message)),
+            Task.Run(() => UDPSend(message))
+            );
+        }
+
+        private static void UDPSend(List<byte[]> message)
+        {
+            try
+            {
+                Console.WriteLine("UDP thread started");
+                UdpClient client = new UdpClient();
+                client.Connect(server, port);
+
+                IPEndPoint host = new IPEndPoint(server, port);
+                IPEndPoint remoteIPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                byte[] buffer = new byte[DefaultBufferSize];
+                System.Text.Encoding.ASCII.GetBytes("SIZE:").Concat(BitConverter.GetBytes(bufferSize)).ToArray().CopyTo(buffer, 0);
+                client.Send(buffer, DefaultBufferSize);
+                Console.WriteLine("UDP Sent buffer size");
+                //client.Client.ReceiveBufferSize = client.Client.SendBufferSize = bufferSize;
+
+                buffer = new byte[bufferSize];
+                buffer = client.Receive(ref remoteIPEndPoint);
+                if (System.Text.Encoding.ASCII.GetString(buffer).Substring(0, 10).Contains("READY"))
+                {
+                    Console.WriteLine("UDP r2g");
+                    foreach (var packet in message)
+                    {
+                        client.Send(packet, bufferSize);
+                        System.Threading.Thread.Sleep(1);
+                    }
+
+                    System.Text.Encoding.ASCII.GetBytes("DONE").CopyTo(buffer, 0);
+                    client.Send(buffer, bufferSize);
+                    Console.WriteLine("UDP DONE");
+                }
+                Console.WriteLine("UDP Thread quitting");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("UDP thread failed!");
+            }
+        }
+
+        private static void TCPSend(List<byte[]> message)
+        {
+            try
+            {
+                Console.WriteLine("TCP thread started");
+                TcpClient client = new TcpClient();
+                client.Connect(server, port);
+
+                Console.WriteLine("TCP Connected");
+                client.Client.NoDelay = useNagle;
+                var session = client.GetStream();
+
+                byte[] buffer = new byte[DefaultBufferSize];
+                System.Text.Encoding.ASCII.GetBytes("SIZE:").Concat(BitConverter.GetBytes(bufferSize)).ToArray().CopyTo(buffer, 0);
+                session.Write(buffer, 0, DefaultBufferSize);
+                Console.WriteLine("TCP sent buffer size");
+                //client.Client.ReceiveBufferSize = client.Client.SendBufferSize = bufferSize;
+
+                buffer = new byte[bufferSize];
+                session.Read(buffer, 0, bufferSize);
+                string data = System.Text.Encoding.ASCII.GetString(buffer);
+                if (System.Text.Encoding.ASCII.GetString(buffer).Substring(0, 10).Contains("READY"))
+                {
+                    Console.WriteLine("TCP r2g");
+                    foreach (var packet in message)
+                    {
+                        session.Write(packet, 0, bufferSize);
+                        System.Threading.Thread.Sleep(1);
+                    }
+                    System.Text.Encoding.ASCII.GetBytes("DONE").CopyTo(buffer, 0);
+                    session.Write(buffer, 0, bufferSize);
+                    Console.WriteLine("TCP DONE");
+                }
+                else
+                {
+                    Console.WriteLine("TCP server is busy!");
+                }
+                Console.WriteLine("TCP thread quitting");
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("TCP thread failed!");
+            }
+        }
+
+        static List<byte[]> PrepareMessage()
+        {
+            var message = new List<byte[]>();
+
+            for (int i = 0; i < messageLengthInBuffers; i++)
+            {
+                message.Add(System.Linq.Enumerable.Repeat((byte)i, bufferSize).ToArray());
+            }
+
+            return message;
+        }
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SpeedTest.Server
+{
+    class Program
+    {
+
+        public static IPAddress Host = IPAddress.Any;
+        public static int Port = 25565;
+        public static int DefaultBufferSize = 1024;
+        public static bool Running = false;
+        static void Main(string[] args)
+        {
+            Task.WaitAll(
+            Task.Run(() => TCPThread()),
+            Task.Run(() => UDPThread())
+            );
+
+        }
+
+        private static void UDPThread()
+        {
+            Console.WriteLine("Started UDPThread");
+            UdpClient client = new UdpClient(Port);
+            IPEndPoint RemoteIPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("UDP Looping");
+                    byte[] buffer = client.Receive(ref RemoteIPEndPoint);
+                    int bufferSize = BitConverter.ToInt32(buffer, 5);
+
+                    Console.WriteLine("UDP got buffer size at {0}", bufferSize);
+                    //client.Client.ReceiveBufferSize = client.Client.SendBufferSize = bufferSize;
+                    buffer = new byte[bufferSize];
+                    System.Text.ASCIIEncoding.ASCII.GetBytes("READY").CopyTo(buffer, 0);
+                    client.Send(buffer, bufferSize, RemoteIPEndPoint);
+
+                    var watch = new System.Diagnostics.Stopwatch();
+                    List<byte[]> message = new List<byte[]>();
+                    Console.WriteLine("Starting to receive UDP");
+                    watch.Start();
+                    client.Client.ReceiveTimeout = 5000;
+                    while (true)
+                    {
+                        //Console.Write(" {0} ", messageCount);
+                        buffer = client.Receive(ref RemoteIPEndPoint);
+                        if (CheckForEndMessage(buffer)) break;
+
+                        byte[] temp = new byte[bufferSize];
+                        Array.Copy(buffer, temp, bufferSize);
+                        message.Add(temp);
+                    }
+                    client.Client.ReceiveTimeout = 0;
+
+                    watch.Stop();
+                    int failures = 0;
+                    int messageCount = 0;
+                    foreach (var b in message)
+                    {
+                        failures += resultChecksum(b, (byte)messageCount);
+                        messageCount++;
+                    }
+
+                    int bytes = bufferSize * messageCount;
+                    double kilobytes = bytes / 1024.00;
+                    double rate = kilobytes / watch.Elapsed.TotalSeconds;
+
+
+                    Console.WriteLine("UDP Thread got {0} kb in {1} s, at a rate of {2} kbps", kilobytes, watch.Elapsed, rate);
+                    Console.WriteLine("Obtained {0} malformed bytes, or {1}% of the message.", failures, 100 * failures / bytes);
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("UDP thread timed out!");
+                    Console.WriteLine(e);
+                    client.Client.ReceiveTimeout = 0;
+                }
+            }
+        }
+
+        private static int resultChecksum(byte[] buffer, byte messageCount)
+        {
+            var failures = buffer.Where(b => b != messageCount).AsParallel();
+            return failures.Count();
+        }
+
+        private static void TCPThread()
+        {
+            TcpListener listener = new TcpListener(Host, Port);
+            listener.Start();
+            Console.WriteLine("Started TCP Thread");
+            while (true)
+            {
+                Console.WriteLine("TCP Looping");
+                TcpClient client = listener.AcceptTcpClient();
+                Task.Run(() => TCPMessageHandler(client));
+            }
+        }
+
+        private static void TCPMessageHandler(TcpClient client)
+        {
+            try
+            {
+                Console.WriteLine("Got TCP Connection");
+                byte[] buffer = new byte[DefaultBufferSize];
+                NetworkStream stream = client.GetStream();
+                stream.Read(buffer, 0, DefaultBufferSize);
+
+                if (Running)
+                {
+                    Console.WriteLine("TCP Busy");
+                    System.Text.ASCIIEncoding.ASCII.GetBytes("BUSY").CopyTo(buffer, 0);
+                    stream.Write(buffer, 0, DefaultBufferSize);
+                    client.Close();
+                    return;
+                }
+                Running = true;
+
+                int bufferSize = BitConverter.ToInt32(buffer, 5);
+                Console.WriteLine("TCP got buffer size at {0}", bufferSize);
+                client.Client.ReceiveBufferSize = client.Client.SendBufferSize = bufferSize;
+                buffer = new byte[bufferSize];
+                System.Text.ASCIIEncoding.ASCII.GetBytes("READY").CopyTo(buffer, 0);
+                stream.Write(buffer, 0, bufferSize);
+
+                bool finish = false;
+                var watch = new System.Diagnostics.Stopwatch();
+                int messageCount = -1;
+                Console.WriteLine("Starting to receive TCP");
+                watch.Start();
+                while (!finish)
+                {
+                    messageCount++;
+                    //Console.Write(" {0} ", messageCount);
+                    stream.Read(buffer, 0, bufferSize);
+                    finish = CheckForEndMessage(buffer);
+                }
+                watch.Stop();
+                client.Close();
+                Running = false;
+                int bytes = bufferSize * messageCount;
+                double kilobytes = bytes / 1024.00;
+                double rate = kilobytes / watch.Elapsed.TotalSeconds;
+                Console.WriteLine("TCP Thread got {0} kb in {1} s, at a rate of {2} kbps", kilobytes, watch.Elapsed, rate);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("UDP thread timed out!");
+                Console.WriteLine(e);
+                client.Close();
+                Running = false;
+            }
+        }
+
+        private static bool CheckForEndMessage(byte[] buffer)
+        {
+            return System.Text.Encoding.ASCII.GetString(buffer).Contains("DONE");
+        }
+    }
+}
